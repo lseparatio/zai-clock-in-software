aef838822fe47f90540d14463ab14abf05d10e75
lseparatio <consultantaanglia@gmail.com>
2022-03-25 10:20:28 +0000

    Test 3...this feature working only on deployed site and android

--- static/js/script.js
+++ static/js/script.js
@@ -15,34 +15,46 @@ if ('NDEFReader' in window) {
 
   navigator.permissions.query({ name: "nfc" }).then((nfcStatus) => {
 
-      if (nfcStatus.state === "granted") {
-          startScanning();
-      } else {
+    if (nfcStatus.state === "granted") {
+      startScanning();
+    } else {
 
-        document.querySelector("#scan").onclick = event => {
-          startScanning();
-        };
+      document.querySelector("#scan").onclick = event => {
+        startScanning();
+      };
 
-      }
+    }
 
   });
 
-  function startScanning(){
-      const ndef = new NDEFReader();
+  function startScanning() {
+    const ndef = new NDEFReader();
 
-      ndef.scan().then(() => {
+    ndef.scan().then(() => {
 
-        ndef.onreadingerror = () => {
-           
-        };
+      ndef.onreadingerror = () => {
 
-        ndef.onreading = event => {
-          console.log(ndef.onreading)
-        };
+      };
 
-      }).catch(error => {
-        /// Something wrong with hardware
-      });
+      ndef.onreading = event => {
+        const message = event.message;
+        for (const record of message.records) {
+          console.log("Record type:  " + record.recordType);
+          console.log("MIME type:    " + record.mediaType);
+          console.log("Record id:    " + record.id);
+          if (record.recordType == "text") {
+
+            const textDecoder = new TextDecoder(record.encoding);
+            let code = textDecoder.decode(record.data);
+            console.log(code);
+
+          }
+        }
+      };
+
+    }).catch(error => {
+      /// Something wrong with hardware
+    });
   }
 
 }
