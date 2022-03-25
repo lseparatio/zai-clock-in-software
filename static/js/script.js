ecddd822e280dba7ec405fb4d53e94d4fdd11fa7
lseparatio <consultantaanglia@gmail.com>
2022-03-25 09:27:20 +0000

    Deleted code that check for user permision

--- static/js/script.js
+++ static/js/script.js
@@ -19,16 +19,3 @@ async function startScanning() {
     /* handle NDEF messages */
   };
 }
-
-const nfcPermissionStatus =  navigator.permissions.query({ name: "nfc" });
-if (nfcPermissionStatus.state === "granted") {
-  // NFC access was previously granted, so we can start NFC scanning now.
-  startScanning();
-} else {
-  // Show a "scan" button.
-  document.querySelector("#scanButton").style.display = "block";
-  document.querySelector("#scanButton").onclick = event => {
-    // Prompt user to allow UA to send and receive info when they tap NFC devices.
-    startScanning();
-  };
-}
