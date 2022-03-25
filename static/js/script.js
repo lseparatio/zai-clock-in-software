cec11fe88943d96a5cb25399326994a07625c69a
lseparatio <consultantaanglia@gmail.com>
2022-03-25 08:27:40 +0000

    Testing

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,23 @@ $(document).ready(function () {
     $('input#username, input#password, input#phone_number').characterCounter();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
-  });
+});
+
+
+const ndef = new NDEFReader();
+if ('NDEFReader' in window) { 
+ndef.scan().then(() => {
+    console.log("Scan started successfully.");
+    ndef.onreadingerror = () => {
+        console.log("Cannot read data from the NFC tag. Try another one?");
+    };
+    ndef.onreading = event => {
+        console.log("NDEF message read. ");
+        alert(`ndef`)
+    };
+}).catch(error => {
+    console.log(`Error! Scan failed to start: ${error}.`);
+});
+}
